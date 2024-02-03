a0d776d9506ba5ed03e57a2da3788ab852e27b11
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2024-02-03 14:44:29 +0000

    Ajax

---
+++ script.js
@@ -0,0 +1,21 @@
+const API_KEY = "9e583e166f7449860811f01adc1ce86e";
+
+$(function(){
+  $('#btn').on('click', function(){
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function (data){
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fail(function (data){
+      alert('通信に失敗しました。');
+    });
+  });
+});
